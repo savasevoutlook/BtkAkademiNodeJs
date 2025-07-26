42b53a52340c695b0691c36f50a5f162271da042
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-26 21:11:22 +0300

    .

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const connection = require('./utility/database');
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +13,14 @@ app.use('/admin', adminRoutes);
 
 const shopRoutes = require('./routes/shop');
 app.use(shopRoutes);
+
+connection.execute('SELECT * FROM products')
+    .then((result) => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 const errorController = require('./controllers/errors');
 app.use(errorController.get404Page);
