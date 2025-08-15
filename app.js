24ed8b0d68ca43f1fe738675823940435ddd92b4
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-08-15 21:16:59 +0300

    mongoose kurulumu

--- app.js
+++ app.js
@@ -6,42 +6,50 @@ const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/errors');
-
-const mongoConnect = require('./utility/database').mongoConnect;
-
 const User = require('./models/user');
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findByEmail('savas.ev@example.com')
-        .then(user => {
-            if (user) {
-                req.user = new User(user.name, user.email, user.cart, user._id);
-                next();
-            }
-        })
-        .catch(err => { console.log(err); });
-});
+// app.use((req, res, next) => {
+//     User.findByEmail('savas.ev@example.com')
+//         .then(user => {
+//             if (user) {
+//                 req.user = new User(user.name, user.email, user.cart, user._id);
+//                 next();
+//             }
+//         })
+//         .catch(err => { console.log(err); });
+// });
+
+const mongoose = require('mongoose');
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404Page);
 
-mongoConnect((client) => {
-    User.findByEmail('savas.ev@example.com')
-        .then(user => {
-            if (!user) {
-                user = new User('savas.ev', 'savas.ev@example.com');
-                return user.save();
-            }
+// mongoConnect((client) => {
+//     User.findByEmail('savas.ev@example.com')
+//         .then(user => {
+//             if (!user) {
+//                 user = new User('savas.ev', 'savas.ev@example.com');
+//                 return user.save();
+//             }
 
-            return user;
-        })
-        .then(() => {
-            app.listen(3000);
-        })
-        .catch(err => { console.log(err); });
-});
+//             return user;
+//         })
+//         .then(() => {
+//             app.listen(3000);
+//         })
+//         .catch(err => { console.log(err); });
+// });
+
+mongoose.connect('mongodb://localhost:27017/node-app')
+    .then(() => {
+        console.log('connected to MongoDB');
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
