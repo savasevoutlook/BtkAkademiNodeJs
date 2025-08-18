3862d87b1777d46636deb7c2f9e4dfef11811615
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-08-18 19:49:55 +0300

    .

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/errors');
-//const User = require('./models/user');
+const User = require('./models/user');
 
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findByEmail('savas.ev@example.com')
-//         .then(user => {
-//             if (user) {
-//                 req.user = new User(user.name, user.email, user.cart, user._id);
-//                 next();
-//             }
-//         })
-//         .catch(err => { console.log(err); });
-// });
+app.use((req, res, next) => {
+    User.findOne({ email: 'savas.ev@example.com' })
+        .then(user => {
+            if (user) {
+                req.user = user;
+                console.log(user);
+                next();
+            }
+        })
+        .catch(err => { console.log(err); });
+});
 
 const mongoose = require('mongoose');
 
@@ -29,25 +30,25 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404Page);
 
-// mongoConnect((client) => {
-//     User.findByEmail('savas.ev@example.com')
-//         .then(user => {
-//             if (!user) {
-//                 user = new User('savas.ev', 'savas.ev@example.com');
-//                 return user.save();
-//             }
-
-//             return user;
-//         })
-//         .then(() => {
-//             app.listen(3000);
-//         })
-//         .catch(err => { console.log(err); });
-// });
-
 mongoose.connect('mongodb://localhost:27017/node-app')
     .then(() => {
         console.log('connected to MongoDB');
+
+        User.findOne({ email: 'savas.ev@example.com' })
+            .then(user => {
+                if (!user) {
+                    user = new User({
+                        username: 'savas.ev',
+                        email: 'savas.ev@example.com'
+                    });
+                    return user.save();
+                }
+                return user;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
         app.listen(3000);
     })
     .catch(err => {
