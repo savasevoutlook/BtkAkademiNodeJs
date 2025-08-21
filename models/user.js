2cc554bd440fada66d3099ae2fc4387d347b75de
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-08-21 13:51:56 +0300

    .

--- models/user.js
+++ models/user.js
@@ -32,4 +32,27 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.methods.addToCart = function(product) {
+    const index = this.cart.items.findIndex(cp => {
+        return cp.productId.toString() === product._id.toString();
+    });
+
+    const updatedCartItems = [...this.cart.items];
+
+    let itemQuantity = 1;
+    if (index >= 0) {
+        itemQuantity = this.cart.items[index].quantity + 1;
+        updatedCartItems[index].quantity = itemQuantity;
+    } else {
+        updatedCartItems.push({
+            productId: product._id,
+            quantity: itemQuantity
+        });
+    }
+
+    this.cart = { items: updatedCartItems };
+
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema); //users
