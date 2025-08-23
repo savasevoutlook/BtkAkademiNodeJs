45beadcfcf6c75c3001eaab0a0c6958ad434ba01
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-08-23 22:48:27 +0300

    .

--- routes/account.js
+++ routes/account.js
@@ -9,11 +9,11 @@ const accountController = require('../controllers/account');
 
 router.get('/login', csrfTokenMiddleware, accountController.getLogin);
 
-router.get('/reset-password', accountController.getResetPassword);
+router.get('/reset-password', csrfTokenMiddleware, accountController.getResetPassword);
 
 router.get('/logout', accountController.getLogout);
 
-router.get('/register', accountController.getRegister);
+router.get('/register', csrfTokenMiddleware, accountController.getRegister);
 
 
 router.post('/login', doubleCsrfProtection, accountController.postLogin);
