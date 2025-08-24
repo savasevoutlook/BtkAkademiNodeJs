cf8c56e4a6b48625fac2f42e25aeb3079f4b7cb0
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-08-24 18:42:41 +0300

    .

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -1,8 +1,11 @@
 module.exports = (req, res, next) => {
+    
     if (!req.user) {
         req.session.redirectTo = req.originalUrl;
         return res.redirect('/login');
-    } else if (!req.user.isAdmin) {
+    }
+    
+    if (!req.user.isAdmin) {
         return res.redirect('/unauthorized');
     }
 
